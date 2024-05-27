8eb4cef41c93f33e896eb7986b4f0948daa7d6b5
Lawrence Leong <45556912+Rexrover2@users.noreply.github.com>
2024-05-27 14:26:19 +1000

    Added example to display error of AppendHttpCallStub when there are query params with curly brackets
    * Added complex parameter logic as example in controller, service and proxy.
    * Re-added previously overwritten test
    * Separated tests to different files

---
+++ Examples/MovieProject/MovieProject.Logic/Extensions/UserMapperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MovieProject.Logic.Proxy.DTO;
+
+namespace MovieProject.Logic.Extensions
+{
+    public static class UserMapperExtensions
+    {
+        public static UserSearchModel MapUserSearchModelDtoToProxyDto(
+            this DTO.MovieProject.Logic.Proxy.DTO.UserSearchModel searchModel)
+        {
+            return new UserSearchModel
+            {
+                Name = searchModel.Name,
+                Username = searchModel.Username,
+                Email = searchModel.Email,
+                Phone = searchModel.Phone,
+                Website = searchModel.Website
+            };
+        }
+    }
+}
--- Examples/MovieProject/MovieProject.Logic/Proxy/UserProxy.cs
+++ Examples/MovieProject/MovieProject.Logic/Proxy/UserProxy.cs
@@ -2,13 +2,17 @@
 using System;
 using System.Net;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using MovieProject.Logic.Proxy.DTO;
 
 namespace MovieProject.Logic.Proxy
 {
     public interface IUserProxy
     {
         Task<DTO.User[]> GetUsers();
+        Task<DTO.User[]> GetSearchUsers(UserSearchModel searchModel);
     }
 
     public class UserProxy  : BaseProxy, IUserProxy
@@ -28,6 +32,22 @@ namespace MovieProject.Logic.Proxy
                 // any exceptions throwm in here will be wrapped inside a DownstreamException, will all the details of the request / response for investigation
                 // also applies the concept of anti-corruption layer, meaning we parse the external DTO inside the proxy, and only return valid / clean internal DTOs
 
+                if(status != HttpStatusCode.OK || users == null)
+                    throw new Exception("Unexpected response");
+
+                return users;
+            });
+
+            return result;
+        }
+        
+        public async Task<DTO.User[]> GetSearchUsers(UserSearchModel searchModel)
+        {
+            string serialisedQueryParameter = JsonSerializer.Serialize(searchModel);
+            string route = $"searchUsers?userSearchModel={serialisedQueryParameter}";
+
+            var result = await Send(HttpMethod.Get, route, (DTO.User[] users, HttpStatusCode status) =>
+            {
                 if(status != HttpStatusCode.OK || users == null)
                     throw new Exception("Unexpected response");
 
