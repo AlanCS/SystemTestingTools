27791a595c6f63555b69d2f7270147b67a06d778
Alan Sarli <alanzis@gmail.com>
2024-05-28 17:58:51 +1000

    adjust folder paths

--- Examples/MovieProject/MovieProject.Tests/MovieProject.IsolatedTests/ComponentTesting/MathHappyTests.cs
+++ Examples/MovieProject/MovieProject.Tests/MovieProject.IsolatedTests/ComponentTesting/MathHappyTests.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using FluentAssertions.Execution;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -30,10 +31,11 @@ namespace IsolatedTests.ComponentTestings
             var client = Fixture.Server.CreateClient();
             client.CreateSession();
 
-            var addResponse = ResponseFactory.FromFiddlerLikeResponseFile($"{Fixture.StubsFolder}/MathWcf/Real_Responses/Happy/200_Add.txt");
+            
+            var addResponse = ResponseFactory.FromFiddlerLikeResponseFile(Path.Combine(Fixture.StubsFolder, "MathWcf/Real_Responses/Happy/200_Add.txt"));
             client.AppendHttpCallStub(HttpMethod.Post, new System.Uri(Url), addResponse, new Dictionary<string, string>() { { "SOAPAction", @"""http://tempuri.org/Add""" } });
 
-            var minusResponse = ResponseFactory.FromFiddlerLikeResponseFile($"{Fixture.StubsFolder}/MathWcf/Real_Responses/Happy/200_Minus.txt");
+            var minusResponse = ResponseFactory.FromFiddlerLikeResponseFile(Path.Combine(Fixture.StubsFolder, "MathWcf/Real_Responses/Happy/200_Minus.txt"));
             client.AppendHttpCallStub(HttpMethod.Post, new System.Uri(Url), minusResponse, new Dictionary<string, string>() { { "SOAPAction", @"""http://tempuri.org/Subtract""" } });
 
             // act
