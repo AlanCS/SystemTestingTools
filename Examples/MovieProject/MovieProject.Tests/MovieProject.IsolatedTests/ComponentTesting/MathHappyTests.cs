63efd8472e80811c163b715ca7d4d62d4bfb155f
Alan Sarli <alanzis@gmail.com>
2024-05-28 18:01:30 +1000

    fixing file path

--- Examples/MovieProject/MovieProject.Tests/MovieProject.IsolatedTests/ComponentTesting/MathHappyTests.cs
+++ Examples/MovieProject/MovieProject.Tests/MovieProject.IsolatedTests/ComponentTesting/MathHappyTests.cs
@@ -30,12 +30,11 @@ namespace IsolatedTests.ComponentTestings
             // arrange
             var client = Fixture.Server.CreateClient();
             client.CreateSession();
-
             
-            var addResponse = ResponseFactory.FromFiddlerLikeResponseFile(Path.Combine(Fixture.StubsFolder, "MathWcf/Real_Responses/Happy/200_Add.txt"));
+            var addResponse = ResponseFactory.FromFiddlerLikeResponseFile($"{Fixture.StubsFolder}/MathWCF/Real_Responses/Happy/200_Add.txt");
             client.AppendHttpCallStub(HttpMethod.Post, new System.Uri(Url), addResponse, new Dictionary<string, string>() { { "SOAPAction", @"""http://tempuri.org/Add""" } });
 
-            var minusResponse = ResponseFactory.FromFiddlerLikeResponseFile(Path.Combine(Fixture.StubsFolder, "MathWcf/Real_Responses/Happy/200_Minus.txt"));
+            var minusResponse = ResponseFactory.FromFiddlerLikeResponseFile($"{Fixture.StubsFolder}/MathWCF/Real_Responses/Happy/200_Minus.txt");
             client.AppendHttpCallStub(HttpMethod.Post, new System.Uri(Url), minusResponse, new Dictionary<string, string>() { { "SOAPAction", @"""http://tempuri.org/Subtract""" } });
 
             // act
